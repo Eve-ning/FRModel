ea5e6e4c3b5fef2938ec64db5c171a3f27c78624
Evening <johnchangqi@hotmail.com>
2022-01-13 21:51:12 +0800

    Update Frame conversion algorithm

--- src/frmodel/base/D3/cloud/_cloud_frame.py
+++ src/frmodel/base/D3/cloud/_cloud_frame.py
@@ -1,10 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
+import alphashape
 import numpy as np
 import utm
 from PIL import Image
-from scipy.interpolate import CloughTocher2DInterpolator
+from PIL import ImageDraw
+from scipy.interpolate import CloughTocher2DInterpolator, griddata
+from scipy.spatial import ConvexHull, Delaunay
+from sklearn.impute import KNNImputer
 
 from frmodel.base.D2 import Frame2D
 
@@ -93,71 +97,22 @@ class _Cloud3DFrame(ABC):
         Y = np.arange(0, shape[1])
         XM, YM = np.meshgrid(X, Y)  # 2D grid for interpolation
 
-        interp_z = CloughTocher2DInterpolator(list(zip(x, y)), z, rescale=True)
+        # This was supposed to use alphashape but it's very not useful
+        # Takes look long and idk why it returns multiple polygons
+        # pts = np.stack([x, y], axis=-1)
+        # hull = alphashape.alphashape(pts[:samples//10], 0.01)
+        # hull_pts = hull.exterior.coords.xy
+        #
+        # img = Image.new('L', (shape[1], shape[0]), 0)
+        # ImageDraw.Draw(img).polygon(np.stack([hull_pts[0], hull_pts[1]], axis=-1),
+        #                             outline=1, fill=1)
+        # mask = np.asarray(img)
+
+        interp_z = CloughTocher2DInterpolator(list(zip(x, y)), z, rescale=True, tol=0.1)
         Z = interp_z(XM, YM)
         Z = np.where(Z < 0, 0, Z)
         Z = np.nan_to_num(Z)
+
         # Not sure why the Y is inverted, something to do with the lat long
-        return Frame2D(Z[:,::-1].T[..., np.newaxis], labels=[Frame2D.CHN.Z])
-    #
-    # def to_frame1(self,
-    #              sample_size=None, transformed=True,
-    #              width=None, height=None,
-    #              method: CONSTS.INTERP3D = CONSTS.INTERP3D.NEAREST,
-    #              clamp_cubic: bool = True) -> Frame2D:
-    #     """ Converts the Cloud3D into a Frame2D
-    #
-    #     :param sample_size: The number of random points to use for interpolation
-    #     :param transformed: Whether to shift axis based on header information
-    #     :param width: Width of resulting image
-    #     :param height: Height of resulting image
-    #     :param method: Method of interpolation, use CONSTS.INTERP3D for various methods
-    #     :param clamp_cubic: Whether to clamp the cubic RGB output or not
-    #     :return: A Frame2D with Z, R, G, B columns
-    #     """
-    #     # Grab array data and scale it down to desired height and width
-    #     ar = self.data(sample_size, transformed)
-    #     height_range  = np.max(ar[..., 0]) - np.min(ar[..., 0])
-    #     width_range = np.max(ar[..., 1]) - np.min(ar[..., 1])
-    #
-    #     if height and not width:
-    #         width = int(width_range / height_range * height)
-    #     elif width and not height:
-    #         height = int(height_range / width_range * width)
-    #     else:
-    #         raise Exception("Frame Height or Width must be specified")
-    #
-    #     ar[..., 0] = (ar[..., 0] - np.min(ar[..., 0])) / height_range * height
-    #     ar[..., 1] = (ar[..., 1] - np.min(ar[..., 1])) / width_range * width
-    #
-    #     # Create grid to estimate
-    #     grid_x, grid_y = np.mgrid[0:height, 0:width]
-    #     grid = grid_x, grid_y
-    #     method: str
-    #     ar_intp = np.zeros(shape=(height, width, 4), dtype=np.float)
-    #
-    #     ar_intp[..., 0] = griddata(ar[..., 0:2], ar[..., 2], grid, method)
-    #
-    #     ar_intp[..., 1] = griddata(ar[..., 0:2], ar[..., 3], grid, method)
-    #     ar_intp[..., 2] = griddata(ar[..., 0:2], ar[..., 4], grid, method)
-    #     ar_intp[..., 3] = griddata(ar[..., 0:2], ar[..., 5], grid, method)
-    #
-    #     if method == CONSTS.INTERP3D.CUBIC and clamp_cubic:
-    #         ar_intp[..., 1:4] = self.interp_sig_clamp(ar_intp[..., 1:4])
-    #
-    #     return Frame2D(ar_intp.swapaxes(0, 1), labels=(CONSTS.CHN.Z, *CONSTS.CHN.RGB))
-    #
-    # @staticmethod
-    # def interp_sig_clamp(x: np.ndarray, alpha: float = 255, beta: float = 50):
-    #     """ Used to clamp the RGB values based on the following formula
-    #
-    #     a / { 1 + exp [ - ( x - a / 2 ) / b ] }
-    #
-    #     :param x: Input
-    #     :param alpha: Amplitude, makes clamping from [0, a]
-    #     :param beta: Bend factor.
-    #     """
-    #
-    #     return alpha / (1 + np.exp( - (x - alpha / 2) / beta))
-    #
-    #
+        f = Frame2D(Z[:,::-1].T[..., np.newaxis], labels=[Frame2D.CHN.Z])
+        return f
