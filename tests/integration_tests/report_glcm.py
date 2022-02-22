b25dc76ff5d6ffb024d2f19521af6b0f553e1859
Evening <johnchangqi@hotmail.com>
2022-02-22 20:57:43 +0800

    Clean up GLCM reporting script

--- tests/integration_tests/report_glcm.py
+++ tests/integration_tests/report_glcm.py
@@ -1,11 +1,10 @@
 import os
 
+from tqdm import tqdm
 import numpy as np
 import pytest
-from matplotlib import pyplot as plt
 
 from frmodel.base.D2 import Frame2D
-from frmodel.data.load import load_spec
 
 DATA_PATH = "C:/Users/johnc/OneDrive - Nanyang Technological University/NTU/URECA/Data/"
 
@@ -16,23 +15,20 @@ DATA_PATH = "C:/Users/johnc/OneDrive - Nanyang Technological University/NTU/UREC
 def test_report(search_path):
     """ Generates the reports for glcms
 
+    This will generate the report for both the histogram and image
+
     :param search_path: Path to search in, can be in subdirs
     :return:
     """
 
-    # ar = Frame2D.load(f"{path}/4")
-
-    for root, dirs, files in os.walk(f"{search_path}/"):
+    for root, dirs, files in tqdm(os.walk(f"{search_path}/")):
         for name in files:
             # Only GLCMs
             if not name.endswith('.npz'): continue
-
             path = os.path.join(root, name)
+            print("Generating Report on ", path)
+            path_no_ext = path[:-4]  # This removes the .npz extension
             f = Frame2D.load(f"{path}")
-            f.plot().hist().savefig("")
-            print(os.path.join(root, name))
-
-    # nans = np.isnan(ar.data.data).any(axis=0).any(axis=0)
-    # plt.imshow(ar.data.data[..., 0])
-    # plt.show()
-    # ar.plot().hist().savefig("out.png")
+            assert not np.isnan(f.data.data).any(), "NaN found in GLCM, aborting"
+            f.plot().hist().savefig(f"{path_no_ext}-hist.jpg")
+            f.plot().image().savefig(f"{path_no_ext}-image.jpg")
